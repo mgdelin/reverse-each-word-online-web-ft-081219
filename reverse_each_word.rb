962fe5f3ae689596f975cd222734d6ae17a1d740
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-15 18:08:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   reversed = []
   split_string = [string.split(" ")]
-  split_string.each do |word|
-    reversed << word.reverse!
+  reversed = [{}]
     
   return reversed
 end
