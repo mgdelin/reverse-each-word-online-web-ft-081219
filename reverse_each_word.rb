19d6b74d47278728acb4314774d5acab3d0e2c3b
Mercedes Delin <mercedesgdelin@gmail.com>
2019-09-15 17:37:22 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string)
+  split_string_array = [string.split("")]
+  reversed = []
+  string.size.times {reversed << split_string_array.pop}
+  reversed.join("")
+  return reversed
+end
+
